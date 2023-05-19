550479c6e7f07bc7d1601ea893f0f3ee1431234e
itslenny <lenny@itslennysfault.com>
2023-05-19 16:22:45 -0400

    first commit

---
+++ bin/index.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+import { TradiverseCli } from '@tradiverse/cli-lib';
+import axios from 'axios';
+
+
+const authToken = process.env.SPACE_AUTH_TOKEN;
+const httpRequest = async r => (await axios(r))?.data;
+
+const cli = new TradiverseCli({ authToken, httpRequest });
+
+const { operationId, result } = await cli.handleCli(process.argv.slice(2));
+
+switch (operationId) {
+
+    // apply custom formatting to cli here based on operationId
+    // case '':
+    //     break;
+
+    default:
+        console.log(typeof result === 'string' ? result : JSON.stringify(result, null, 2));
+}
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Tradiverse CLI
+
+A automatically generated CLI for [Space Traders](https://spacetraders.io/)
+
+<img src="cli-demo.gif">
+
+## Usage
+
+```bash
+npm install -g @tradiverse/cli
+
+tradiverse help
+```
